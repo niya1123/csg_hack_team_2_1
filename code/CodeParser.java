f62d4883d74628bcdbd0d075a0e3aa8384d7d077
niya1123 <yoheikurioka@gmail.com>
2019-08-26 21:08:46 +0900

    やる気なくした

---
+++ code/CodeParser.java
@@ -0,0 +1,13 @@
+/**
+ * CodeParser
+ */
+public class CodeParser {
+
+    private FileScanner fileScanner;
+
+    CodeParser(String sourceFileName){
+        fileScanner = new FileScanner(sourceFileName);
+    }
+
+    
+}
